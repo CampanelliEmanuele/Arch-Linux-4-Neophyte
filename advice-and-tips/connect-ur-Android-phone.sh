a57c1092e2eb3ba19b861f5546234045d54c74a5
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2021-11-03 16:14:54 +0100

    add: script 2 see ur android phone's memory

---
+++ advice-and-tips/connect-ur-Android-phone.sh
@@ -0,0 +1,30 @@
+: '
+    script to allow you to play in your phone s memory
+
+    To run this file open ur terminal, go in this folder and run:
+        chmod +x connect-ur-Android-phone.sh
+        ./connect-ur-Android-phone.sh
+
+    !!! final reboot, be careful !!!
+    !!! final reboot, be careful !!!
+    !!! final reboot, be careful !!!
+    !!! final reboot, be careful !!!
+    !!! final reboot, be careful !!!
+    !!! final reboot, be careful !!!
+    !!! final reboot, be careful !!!
+    !!! final reboot, be careful !!!
+'
+
+sudo pacman -Syu
+sudo pacman -S git
+sud pacman -S mtpfs
+cd
+cd Downloads/
+git clone https://aur.archlinux.org/jmtpfs.git
+cd jmtpfs/
+makepkg -s
+sudo pacman -U jmtpfs-0.5-2-x86_64.pkg.tar.zst
+sudo pacman -Sy gvfs-mtp
+sudo pacman -Sy gvfs-gphoto2
+sudo reboot     #  !!! final reboot, be careful !!!
+
