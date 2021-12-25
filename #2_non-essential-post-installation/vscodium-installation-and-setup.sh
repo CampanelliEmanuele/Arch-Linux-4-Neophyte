a3096274f75d10f002aefccf8c52b6f5161e68ee
Campanelli Emanuele <emanuele.campanelli@studio.unibo.it>
2021-12-25 15:01:18 +0100

    Add: new theme added

--- #2_non-essential-post-installation/vscodium-installation-and-setup.sh
+++ #2_non-essential-post-installation/vscodium-installation-and-setup.sh
@@ -44,7 +44,8 @@
   3) EXTENSIONS
     
     3.1 Themes:
-      3.1.1. Synthax
-      3.1.2 Ufo Theme
-      3.1.3 Night Coder
+      3.1.1 Synthax
+      3.1.2 Tokyo Night
+      3.1.3 Ufo Theme
+      3.1.4 Night Coder
 '
